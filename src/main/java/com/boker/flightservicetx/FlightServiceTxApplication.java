333d021ff46424c88a729712c38ba2d64b21a0a9
bokerwere <josephboker93@gmai.com>
2022-04-20 12:02:58 +0300

    swagger2

--- src/main/java/com/boker/flightservicetx/FlightServiceTxApplication.java
+++ src/main/java/com/boker/flightservicetx/FlightServiceTxApplication.java
@@ -2,8 +2,10 @@ package com.boker.flightservicetx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class FlightServiceTxApplication {
 
 	public static void main(String[] args) {
